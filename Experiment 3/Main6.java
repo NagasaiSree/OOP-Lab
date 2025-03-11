76b50cf41087dcd6174c03696f47d0be1d1bbd99
NagasaiSree <nagasaisreegummadi@gmail.com>
2025-03-11 09:01:39 +0530

    Add files via upload

---
+++ Experiment 3/Main6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main6{
+public static void main(String[] args){
+int dec_num, quot, i = 1, j;
+int bin_num[] = new int[100];
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter a decimal number");
+dec_num = scan.nextInt();
+quot = dec_num;
+while (quot!=0){
+bin_num[i++] = quot % 2;
+quot = quot / 2;
+}
+System.out.println("Binary number is ");
+for (j = i-1; j>0; j--) {
+System.out.println(bin_num[j]);
+}
+System.out.println("\n");
+}
+}
---
+++ Experiment 3/Main8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main8 {
+public static void main(String[] args) {
+int n;
+char s1, s2, s3;
+Scanner x = new Scanner(System.in);
+System.out.println("Enter number:");
+n = x.nextInt();
+System.out.printf("%d + %d%d + %d%d%d\n", n, n, n, n, n, n);
+}
+}
